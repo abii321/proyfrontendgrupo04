a9a9dfd5a9d03f41a4fba44c34b774cac179d863
Christian <christianguari@gmail.com>
2026-06-29 20:47:45 -0300

    Solicitud de ayuda yr espuesta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { LoginComponent } from './components/login/login.component';
+import { SolicitudAyudaComponent } from './components/solicitud-ayuda/solicitud-ayuda.component/solicitud-ayuda.component';
+import { RespuestaAyudaComponent } from './components/respuesta-ayuda/respuesta-ayuda.component/respuesta-ayuda.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'registro', component: RegistroComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'solicitud-ayuda', component: SolicitudAyudaComponent },
+    { path: 'solicitudes/:id', component: RespuestaAyudaComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' },
+    
 ];
--- src/app/components/solicitud-ayuda/solicitud-ayuda.component/solicitud-ayuda.component.ts
+++ src/app/components/solicitud-ayuda/solicitud-ayuda.component/solicitud-ayuda.component.ts
@@ -1,9 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Solicitud } from '../../../models/solicitud.class';
+import { SolicitudService } from '../../../services/solicitud-ayuda.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-solicitud-ayuda.component',
-  imports: [],
+  selector: 'app-solicitud-ayuda',
   templateUrl: './solicitud-ayuda.component.html',
-  styleUrl: './solicitud-ayuda.component.css',
+  styleUrls: ['./solicitud-ayuda.component.css'],
+  imports: [CommonModule, FormsModule],
+  standalone: true
 })
-export class SolicitudAyudaComponent {}
+
+export class SolicitudAyudaComponent implements OnInit{
+
+  solicitudes:Solicitud[]=[];
+
+  solicitud:Solicitud=new Solicitud();
+
+  constructor(
+    private solicitudService:SolicitudService
+  ){}
+
+  ngOnInit(){
+
+    this.cargarSolicitudes();
+
+  }
+
+  cargarSolicitudes(){
+
+    this.solicitudService.getSolicitudes().subscribe({
+
+      next:(result)=>{
+
+        this.solicitudes=result.data;
+
+      },
+
+      error:(err)=>{
+
+        console.log(err);
+
+      }
+
+    });
+
+  }
+    guardar() {
+      console.log('Solicitud enviada:', this.solicitud);
+      this.solicitudService.createSolicitud(this.solicitud).subscribe({
+        next: (result) => {
+          alert(result.msg);
+          this.solicitud = new Solicitud();
+          this.cargarSolicitudes();
+        },
+        error: (err) => console.error('Error al crear solicitud', err)
+      });
+    }
+
+  eliminar(id:number){
+
+    this.solicitudService.deleteSolicitud(id).subscribe({
+
+      next:()=>{
+
+        this.cargarSolicitudes();
+
+      }
+
+    });
+
+  }
+
+}
