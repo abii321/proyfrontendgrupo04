73f4daf7e52a8b41b5592e168738c904ffb1c3c3
Areyss <arielryss@gmail.com>
2026-07-04 17:38:09 -0300

    feat: componente chatbot

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/layout/header/header.component";
 import { FooterComponent } from "./components/layout/footer/footer.component";
+import { ChatComponent } from "./components/chat/chat.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ChatComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
