627a28efcd34495ac16e7c3ab665b84f37a926f3
abii321 <abigail.teran321@gmail.com>
2026-07-07 17:10:46 -0300

    modiiacion vista navbar segun rol

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,18 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AutenticacionService } from '../../../services/autenticacion.service';
-import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive, CommonModule],
+  imports: [ RouterLink, RouterLinkActive ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   autenticacionService = inject(AutenticacionService);
-
-  rolUsuario = sessionStorage.getItem("usuario")? JSON.parse(sessionStorage.getItem("usuario")!).rol : 'null';
-
 }
